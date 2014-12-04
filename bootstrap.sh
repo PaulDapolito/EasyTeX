590d88883c6458633be949fbc332cf321fbec68b
Paul Dapolito <pdapolito@hmc.edu>
2014-12-03 21:40:48 -0800

    fixed bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,19 @@
 
 echo "Bootstrapping EasyTeX"
 
+# Delete previous virtual environment
+if [ -d "/usr/local/venv" ]; then
+  rm -rf /usr/local/venv
+fi
+
 echo "Installing virtualenv"
 pip install virtualenv
 
 echo "Creating venv"
-virtualenv venv
+virtualenv /usr/local/venv
 
 # Activate virtual environment and install necessary packages
-source venv/bin/activate
+source /usr/local/venv/bin/activate
 pip install pyparsing
 pip install coverage
 pip install nose
--- easytex.sh
+++ easytex.sh
@@ -3,7 +3,7 @@
 echo "Running EasyTeX!"
 
 # Usage: ./easytex easytex_file_name
-source venv/bin/activate
+source /usr/local/venv/bin/activate
 source/easytex.py $1
 
 echo "EasyTeX completed computation!"
