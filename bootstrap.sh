0283d67eb1e6dadd98b858ac0eaf4f6887de8a55
Paul Dapolito <pdapolito@hmc.edu>
2014-12-03 23:57:53 -0800

    Added fix to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,19 @@
 echo "Bootstrapping EasyTeX"
 
 # Delete previous virtual environment
-if [ -d "/usr/local/venv" ]; then
-  rm -rf /usr/local/venv
+if [ -d "/usr/local/EasyTeX/venv" ]; then
+  rm -rf /usr/local/EasyTeX/venv
 fi
 
 echo "Installing virtualenv"
 pip install virtualenv
 
 echo "Creating venv"
-virtualenv /usr/local/venv
+virtualenv /usr/local/EasyTeX/venv
 
 # Activate virtual environment and install necessary packages
-source /usr/local/venv/bin/activate
+source /usr/local/EasyTeX/venv/bin/activate
 pip install pyparsing
 pip install coverage
 pip install nose
+pip install tex
