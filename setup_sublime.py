3e302314438b9142d5f1e2b94a3d0adff43fd4bb
Paul Dapolito <pdapolito@hmc.edu>
2015-01-11 19:53:32 -0500

    Updated sublime setup script to avoid overwriting source sublime-build file

--- setup_sublime.py
+++ setup_sublime.py
@@ -9,17 +9,23 @@ def main():
     os.system("mkdir ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/EasyTeX")
     os.system("cp EasyTex\ Grammar.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/EasyTex")
 
-    # Configure Sublime build file
-    build_file = open("EasyTeX.sublime-build", "r+")
+    # Open and load Sublime build file
+    build_file_name = "EasyTeX.sublime-build"
+    build_file = open(build_file_name, "r+")
     build_config = json.loads(str(build_file.read()))
-    project_path = os.path.dirname(os.path.realpath(__file__))
-    build_config["working_dir"] = project_path
-    build_file.seek(0)
-    build_file.write(json.dumps(build_config))
     build_file.close()
 
-    # Copy Sublime build file
-    os.system("cp EasyTeX.sublime-build ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User")
+    # Add correct working directory to Sublime build configuration
+    project_path = os.path.dirname(os.path.realpath(__file__))
+    build_config["working_dir"] = project_path
+
+    # Write Sublime build configuration file
+    updated_build_file = open(build_file_name + ".tmp", "w+")
+    updated_build_file.write(json.dumps(build_config))
+    updated_build_file.close()
+
+    # Move Sublime build configuration file to Sublime User Packages directory
+    os.system("mv EasyTeX.sublime-build.tmp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/EasyTeX.sublime-build")
 
     print "Sublime Text 2 syntax highlighting and EasyTeX build have been configured. \
            Please restart Sublime Text 2 to see these changes."
